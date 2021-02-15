81b16c3cf7d6d744ee5f0658f16cac2a59fbbbb7
IKoshelev <koshelew@live.com>
2021-02-15 21:41:48 +0100

    Added playground

---
+++ Playground/OutputWriterHandlerPipeWriter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using MMALSharp.Common;
+using MMALSharp.Common.Utility;
+using MMALSharp.Handlers;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamSplitterExperiment
+{
+    public class OutputWriterHandlerPipeWriter : OutputCaptureHandler, IVideoCaptureHandler
+    {
+        private readonly PipeWriter PipeWriter;
+        public Action? OnPostProcess;
+        public bool PauseWritingTillIframe = false;
+        public OutputWriterHandlerPipeWriter(PipeWriter pipeWriter)
+        {
+            this.PipeWriter = pipeWriter;
+        }
+
+        public override void Dispose()
+        {
+            MMALLog.Logger.LogInformation($"Successfully processed {Helpers.ConvertBytesToMegabytes(this.Processed)}.");
+            this.PipeWriter.Complete();
+        }
+
+        public override void PostProcess()
+        {
+            this.OnPostProcess?.Invoke();
+        }
+
+        public override void Process(ImageContext context)
+        {
+            //_ = this.PipeWriter.WriteAsync(context.Data, this.CancelationToken).Result;
+            if (this.PauseWritingTillIframe)
+            {
+                if (context.IFrame == false)
+                {
+                    return;
+                }
+                else
+                {
+                    this.PauseWritingTillIframe = false;
+                }
+            }
+
+            this.PipeWriter.AsStream().Write(context.Data, 0, context.Data.Length);
+        }
+
+        public void Split()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override string TotalProcessed()
+        {
+            return $"{Helpers.ConvertBytesToMegabytes(this.Processed)}";
+        }
+    }
+}
---
+++ Playground/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.Pipelines;
+using System.Buffers;
+using MMALSharp;
+using MMALSharp.Handlers;
+using MMALSharp.Components;
+using MMALSharp.Ports;
+using MMALSharp.Common;
+
+namespace StreamSplitterExperiment
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var symaphore = new Symaphore();
+
+            Console.WriteLine("running");
+            TcpListener server1 = new TcpListener(IPAddress.Any, 9010);
+            NetworkStream? activeClient1 = null;
+            server1.Start();
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    //we are responsible for never having more than 1 client
+                    activeClient1 = (await server1.AcceptTcpClientAsync()).GetStream();
+                    symaphore.StreamVideo = true;
+                    var requestIFrame = symaphore.RequestIFrame;
+                    requestIFrame?.Invoke();
+                    Console.WriteLine("Connected 1");
+                }
+            });
+
+            TcpListener server2 = new TcpListener(IPAddress.Any, 9011);
+            NetworkStream? activeClient2 = null;
+            server2.Start();
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    //we are responsible for never having more than 1 client
+                    activeClient2 = (await server2.AcceptTcpClientAsync()).GetStream();
+                    symaphore.StreamPhoto = true;
+                    Console.WriteLine("Connected 2");
+                }
+            });
+
+            var pipeH264 = new Pipe();
+            var writerH264 = new OutputWriterHandlerPipeWriter(pipeH264.Writer);
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    ReadResult result = await pipeH264.Reader.ReadAsync();
+                    ReadOnlySequence<byte> buffer = result.Buffer;
+
+                    if (activeClient1 != null)
+                    {
+                        //Console.WriteLine($"pipeH264 write {buffer.Length}");
+                        await activeClient1.WriteAsync(buffer.ToArray());
+                        await activeClient1.FlushAsync();
+                    }
+
+                    // advance even when no client, to avoid piple-up
+                    pipeH264.Reader.AdvanceTo(buffer.End);
+                }
+            });
+
+            var pipeMJPEG = new Pipe();
+            var writerMJPEG = new OutputWriterHandlerPipeWriter(pipeMJPEG.Writer);
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (activeClient2 != null)
+                    {
+                        ReadResult result = await pipeMJPEG.Reader.ReadAsync();
+                        ReadOnlySequence<byte> buffer = result.Buffer;
+
+                        Console.WriteLine($"pipeMJPEG write {buffer.Length}");
+                        await activeClient2.WriteAsync(buffer.ToArray());
+                        await activeClient2.FlushAsync();
+
+                        // advance even when no client, to avoid piple-up
+                        pipeMJPEG.Reader.AdvanceTo(buffer.End);
+                    }
+                    else
+                    {
+                        await Task.Delay(100);
+                    }
+                }
+            });
+
+            var token = new CancellationTokenSource().Token;
+            await CaptureDifferentStreams(writerH264, writerMJPEG, symaphore, token);
+
+        }
+
+        public static async Task CaptureDifferentStreams(
+            OutputWriterHandlerPipeWriter h264PipeWriter,
+            OutputWriterHandlerPipeWriter mjpegPipeWriter,
+            Symaphore symaphore,
+            CancellationToken token = default)
+        {
+            MMALCamera cam = MMALCamera.Instance;
+
+            MMALCameraConfig.InlineHeaders = true;
+
+            cam.ConfigureCameraSettings();
+
+            await Task.Delay(2000);
+
+            symaphore.StreamVideo = false;
+            using (var vidEncoder = new MMALVideoEncoder())
+            {
+                var portConfig = new MMALPortConfig(MMALEncoding.H264, MMALEncoding.I420, 25, 0, null);
+
+                vidEncoder.ConfigureOutputPort(portConfig, h264PipeWriter);
+
+                // Create our component pipeline.
+                cam.Camera.VideoPort.ConnectTo(vidEncoder);
+                //this.Camera.PreviewPort.ConnectTo(renderer);
+
+                symaphore.RequestIFrame = () =>
+                {
+                    Console.WriteLine("Requesting IFrame");
+                    vidEncoder.RequestIFrame();
+                    h264PipeWriter.PauseWritingTillIframe = true;
+                };
+
+                var source = new CancellationTokenSource(TimeSpan.FromSeconds(100));
+                Console.WriteLine($"starting vid stream");
+                await cam.ProcessAsync(cam.Camera.VideoPort, source.Token).ConfigureAwait(false);
+                Console.WriteLine($"ending vid stream");
+            }
+
+            cam.Cleanup();
+        }
+    }
+
+    public class Symaphore
+    {
+        public bool StreamVideo = false;
+        public Action? RequestIFrame = null;
+        public bool StreamPhoto = false;
+    }
+}
